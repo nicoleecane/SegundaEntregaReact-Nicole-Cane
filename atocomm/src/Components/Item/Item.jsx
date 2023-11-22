51548cffd0e2bce9d5f1a4d45d91191e1915c3be
NicoleCane <nicolee.c1993@gmail.com>
2023-11-22 21:55:10 +0800

    corrigiendo error pag item

--- atocomm/src/Components/Item/Item.jsx
+++ atocomm/src/Components/Item/Item.jsx
@@ -1,13 +1,18 @@
-import { Item } from "../Item/Item"
+import { Link } from "react-router-dom";
 
-export const ItemList = ({products}) => {
+export const Item = ({id, name, img, }) => {
   return (
-    <>
-    <div className="d-flex flex-wrap">
-
-        {products.map( product => <Item key={product.id} {...product}  />)}
+    <div className="border m-2">
+      <div className="card ">
+        <div className="card-body ">
+          <h5 className="card-title">{name}</h5>
+          <img src={img} alt="" />
+          <Link to={`/item/${id}`}>See more</Link>
+          <Link to={`/item/${id}`}>
+            <button className="btn btn-outline-success">Detalles</button>
+          </Link>
+        </div>
+      </div>
     </div>
-        
-    </>
-  )
-}
+  );
+};
